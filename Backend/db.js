3a00e0efd484f554c71d0a86d91cc5d1b3aecfa6
nishchyapratapsingh <np1416@srmist.edu.in>
2025-07-04 15:29:26 +0530

    express server setup

---
+++ Backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb://localhost:27017';
+
+const connectToMongo =async()=>{
+    try{
+        await mongoose.connect(mongoURI);
+        console.log("Mongo connected successfully");
+    }
+    catch(error) {
+        console.error("Error: ", error); 
+    }
+}
+
+module.exports = connectToMongo;
