bc0791774bc5540a9d6c16909aa950061ad2dd28
nishchyapratapsingh <np1416@srmist.edu.in>
2025-07-05 19:15:00 +0530

    express validator

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,10 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
+const User = require("../models/User");
+const { check, validationResult } = require("express-validator");
 
-router.post('/', (req, res)=>{
-    const user = User(req.body);
-    user.save();
-})
+router.post(
+  "/",
+  check("email").isEmail(),
+  check("name").isLength({ min: 3 }),
+  check("password").isLength({ min: 8 }),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+
+    try {
+      const user = User(req.body);
+      await user.save();
+      res.status(201).json({ success: "User created", user});
+    } catch (error) {
+      if (error.code === 11000) {
+        res.status(400).json({ error: "Email already in use" });
+      } else {
+        res.status(500).json({ error: "Internal server error" });
+      }
+    }
+  }
+);
 
 module.exports = router;
