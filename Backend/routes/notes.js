2f4ab07c6f7be7731e4b3cc9e39b37114da53a16
nishchyapratapsingh <np1416@srmist.edu.in>
2025-08-06 23:51:22 +0530

    Add update note functionality with validation and error handling

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -3,6 +3,7 @@ const fetchuser = require("../middlewares/fetchuser");
 const Notes = require("../models/Notes");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
+const { set } = require("mongoose");
 
 router.get("/fetchallnotes", fetchuser, async (req, res) => {
   const notes = await Notes.find({ user: req.userId });
@@ -32,5 +33,34 @@ router.post(
     }
   }
 );
+
+router.put(  //put req for updation
+  "/updatenote/:id",
+  fetchuser,
+  async (req, res) => {
+    try {
+      const {title, description, tag} =req.body;
+      const newNote = {};
+      if (title) {newNote.title = title};
+      if (description) {newNote.description = description};
+      if (tag) {newNote.tag = tag};
+
+      let note = await Notes.findById(req.params.id);
+      if (!note) {
+        return res.status(404).send("Not Found");
+      }
+      if ((note.user.toString()) != req.userId) {
+        return res.status(401).send("Not Found");
+      }
+      note = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true} )
+      res.status(200).json({ success: "note updated", title: req.body.title });
+    } catch (error) {
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
+);
+
+
+
 
 module.exports = router;
