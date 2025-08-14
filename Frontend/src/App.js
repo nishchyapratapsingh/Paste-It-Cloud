698975b92cb11ac50fbed471b64c36c0638ec797
nishchyapratapsingh <np1416@srmist.edu.in>
2025-08-14 22:33:25 +0530

    Add Bootstrap integration and create Navbar, Home, and About components

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,8 +1,23 @@
 import "./App.css";
+import {
+    BrowserRouter as Router,
+    Routes, Route
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="container">New App</div>
+    <Router>
+      <Navbar />
+      <div className="container"> 
+        <Routes>
+          {/* <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
