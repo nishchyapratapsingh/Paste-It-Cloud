270b020e4f5b9bfd7892eb19b0948f10a3e8a978
nishchyapratapsingh <np1416@srmist.edu.in>
2025-08-19 23:36:26 +0530

    minor changes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Navbar />
       <div className="container"> 
         <Routes>
-          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/" element={<Home />} />
           <Route exact path="/about" element={<About />} />
         </Routes>
       </div>
